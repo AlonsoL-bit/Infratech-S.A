8505917aa8e2c451b851667558343798c0efdd91
alonso <alabbel@correo.uss.cl>
2025-07-03 23:12:04 -0400

    Update urls.py

--- backend/infratech_backend/urls.py
+++ backend/infratech_backend/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('incidents.urls')),
+    path('', lambda request: redirect('admin/', permanent=False)),
 ]
+
 
